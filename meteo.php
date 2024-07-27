87b0f733b7012b8f9dc5e4911edae0cb9595580f
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-07-27 02:37:16 +0200

    Update meteo.php

--- meteo.php
+++ meteo.php
@@ -38,7 +38,7 @@ if(mysqli_connect_errno()){
 echo "wystąpił błąd połączenia z bazą";
 }
 $wynik=mysqli_query($baza,"SELECT * FROM `pogoda` WHERE `miasta_id`=2 ORDER BY `data_prognozy` DESC");
-           while($row = mysqli_fetch_array($wynik)){
+while($row = mysqli_fetch_array($wynik)){
                
             echo "<tr>";
             echo "<td>";
