0a3273ef329d8704c074ccd419a1d1cd7c036cf0
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-07-28 13:19:26 +0200

    Update meteo.php

--- meteo.php
+++ meteo.php
@@ -52,7 +52,7 @@ echo $row['temperatura_noc'];"<br>";
 echo "</td>";
 echo "<td>";
             echo $row['temperatura_dzien'];"<br>";
-            echo "</td>";
+echo "</td>";
 echo "<td>";
 echo $row['opady'];"<br>";
 echo "</td>";
